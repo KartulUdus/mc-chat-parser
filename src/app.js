1e407c863e7810ca030361ce635bc5bb94b46edd
derek-maekask <derek.maekask@vntrs.com>
2023-10-12 17:30:05 +0300

    tail log files instead of container logs

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const Docker = require('dockerode')
 const docker = new Docker()
 const container = docker.getContainer(process.env.DOCKER_CONTAINER_NAME)
 const { Client, Events, Partials, GatewayIntentBits } = require('discord.js')
+const TailFile = require('@logdna/tail-file')
 
 const client = new Client({ 	
 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
@@ -11,24 +12,32 @@ const client = new Client({
 let parseDocker = false
 
 const readDockerLogs = () => {
-  container.logs({ follow: true, stdout: true, stderr: true }, (err, stream) => {
-    stream.on('data', (chunk) => {
-      const logOutput = chunk.toString('utf8')
-			if(logOutput.match(/\[Server thread\/INFO]: </) && parseDocker) {
-				const message = logOutput.substring(logOutput.indexOf('>') + 1)
-				const user = logOutput.match(/<\w+>/)[0].replace(/[<>]/g, '')
-				const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID)
-				channel.send({
-					embeds: [{
-						description: message,
-						author: {
-							name: user,
-							icon_url: `https://minotar.net/helm/${user}/150.png`
-						}
-					}]
-				})
-			}
-    })
+	new TailFile('/data/logs/server.log', {encoding: 'utf8'})
+  .on('data', (logOutput) => {
+		if(logOutput.match(/\[Server thread\/INFO]: </) && parseDocker) {
+			const message = logOutput.substring(logOutput.indexOf('>') + 1)
+			const user = logOutput.match(/<\w+>/)[0].replace(/[<>]/g, '')
+			const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID)
+			channel.send({
+				embeds: [{
+					description: message,
+					author: {
+						name: user,
+						icon_url: `https://minotar.net/helm/${user}/150.png`
+					}
+				}]
+			})
+		}
+	})
+  .on('tail_error', (err) => {
+    console.error('TailFile had an error!', err)
+  })
+  .on('error', (err) => {
+    console.error('A TailFile stream error was likely encountered', err)
+  })
+  .start()
+  .catch((err) => {
+    console.error('Cannot start.  Does the file exist?', err)
   })
 }
 
